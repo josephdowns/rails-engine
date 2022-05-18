4ead41d8c6b4772290258c67e9e6bec5b9dddb6b
Joseph Downs <jos.w.downs@gmail.com>
2022-05-18 13:14:42 -0600

    Update an item

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -2,4 +2,17 @@ class Api::V1::ItemsController < ApplicationController
   def index
     render json: ItemSerializer.new(Item.all)
   end
+
+  def show
+    render json: ItemSerializer.new(Item.find(params[:id]))
+  end
+
+  def update
+    render json: ItemSerializer.new(Item.update(params[:id], item_params))
+  end
+
+  private
+    def item_params
+      params.require(:item).permit(:name, :description, :unit_price, :merchant_id)
+    end
 end
--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -1,18 +1,17 @@
 require 'rails_helper'
 
 describe "Items API" do
-  before(:each) do
+
+  it "sends a list of all items" do
     create_list(:item, 5)
     get '/api/v1/items'
 
-    @response_body = JSON.parse(response.body, symbolize_names: true)
-    @items = @response_body[:data]
-  end
-  it "sends a list of all items" do
+    response_body = JSON.parse(response.body, symbolize_names: true)
+    items = response_body[:data]
 
     expect(response).to be_successful
 
-    @items.each do |item|
+    items.each do |item|
       expect(item).to have_key(:id)
       expect(item).to have_key(:attributes)
 
@@ -29,5 +28,46 @@ describe "Items API" do
       expect(item[:attributes][:merchant_id]).to be_a(Integer)
     end
 
+  end
+
+  it "send one item by id" do
+    id = create(:item).id
+
+    get "/api/v1/items/#{id}"
+
+    response_body = JSON.parse(response.body, symbolize_names: true)
+    item = response_body[:data]
+
+    expect(response).to be_successful
+
+    expect(item).to have_key(:id)
+    expect(item).to have_key(:attributes)
+
+    expect(item[:attributes]).to have_key(:name)
+    expect(item[:attributes][:name]).to be_a(String)
+
+    expect(item[:attributes]).to have_key(:description)
+    expect(item[:attributes][:description]).to be_a(String)
+
+    expect(item[:attributes]).to have_key(:unit_price)
+    expect(item[:attributes][:unit_price]).to be_a(Float)
+
+    expect(item[:attributes]).to have_key(:merchant_id)
+    expect(item[:attributes][:merchant_id]).to be_a(Integer)
+
+  end
+
+  it "can update an item" do
+    id = create(:item).id
+    previous_name = Item.last.name
+    item_params = { name: "Special Spork" }
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    patch "/api/v1/items/#{id}", headers: headers, params: JSON.generate({item: item_params})
+    item = Item.find_by(id: id)
+
+    expect(response).to be_successful
+    expect(item.name).to_not eq(previous_name)
+    expect(item.name).to eq("Special Spork")
   end
 end
