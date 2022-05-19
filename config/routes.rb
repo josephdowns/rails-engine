6a28e74606bbed6bef32a9a397949fcd8291d28a
Joseph Downs <jos.w.downs@gmail.com>
2022-05-18 23:24:46 -0600

    Create search method

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      get "/merchants/find", to: 'search#index'
       resources :merchants, only: [:index, :show] do
         resources :items, only: [:index], controller: 'merchant_items'
       end
+      # namespace :merchants do
+      #   resources :find, only: [:index], controller: 'search'
+      # end
       resources :items do
         resources :merchant, only: [:index], controller: 'item_merchant'
       end
